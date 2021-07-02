e4a25a8532d8325a3cc40bac564c384a9c816d9e
utkarsh-101-tech <71766166+utkarsh-101-tech@users.noreply.github.com>
2021-07-02 12:18:52 +0530

    Add files via upload

---
+++ umbrella.js
@@ -0,0 +1,15 @@
+class Umbrella {
+    constructor(x, y) {
+        var options = {
+            'isStatic': true
+        }
+        this.body = Bodies.circle(x + 10, y - 67, 90, options);
+        World.add(world, this.body);
+    }
+
+    display() {
+        fill("green")
+        var pos = this.body.position;
+        ellipse(pos.x, pos.y, 180, 180);
+    }
+}
